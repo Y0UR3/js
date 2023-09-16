5c5fcf6c244fbefb810ae3f9c4d481d8439fe034
Yuri <66449065+Y0UR3@users.noreply.github.com>
2023-09-16 19:09:25 -0300

    Create index.js

---
+++ exercicios/ex004/index.js
@@ -0,0 +1,20 @@
+console.log(5 + 5);
+console.log("5 + 5");
+
+/* SEM AS ASPAS DUPLAS OU SIMPLES O JS INTERPRETA COMO NUMEROS INTEIROS
+   COM AS ASPAS O JS INTERPRETA COMO PALAVRAS
+-----------------------------------------------------------------------------
+   Adição (+): Este operador é usado para adicionar dois valores.
+
+   Subtração (-): Este operador é usado para subtrair um valor de outro.
+
+   Multiplicação (*): Este operador é usado para multiplicar dois valores. 
+
+   Divisão (/): Este operador é usado para dividir um valor pelo outro.
+
+   Módulo (%): Este operador retorna o resto da divisão entre dois valores.
+   
+   Incremento (++) e Decremento (--): Esses operadores são usados para aumentar ou diminuir o valor de uma variável em 1.
+
+   Operador de Exponenciação ():** Este operador realiza a exponenciação, elevando um número à potência de outro.
+*/
